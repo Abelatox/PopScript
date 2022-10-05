879643b3c352724d05bd9a48ee57500069a475de
Abelatox <abel54225@gmail.com>
2022-10-05 17:55:58 +0200

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -3,8 +3,9 @@
 #Pop!_OS 22.04
 #Wine
 dpkg --add-architecture i386 &&
-wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources &&
-mv winehq-jammy.sources /etc/apt/sources.list.d &&
+#wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources &&
+#mv winehq-jammy.sources /etc/apt/sources.list.d &&
+wget -nc -O /etc/apt/sources.list.d/winehq-jammy.sources https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources &&
 
 #Curtail
 add-apt-repository -y ppa:apandada1/curtail &&
@@ -16,7 +17,7 @@ apt update &&
 apt upgrade -y &&
 
 apt install -y --install-recommends winehq-devel &&
-apt install -y winetricks exe-thumbnailer curtail &&
+apt install -y winetricks exe-thumbnailer curtail arronax &&
 
 #Games
 apt install -y steam pcsx2 &&
@@ -24,15 +25,13 @@ apt install -y steam pcsx2 &&
 #Apache
 apt install -y apache2 php
 
-
 #Other programs
-apt install -y --ignore-missing gnome-tweaks gnome-shell-extensions cowsay lolcat neofetch htop discord curtail arronax tilix cura gnome-sushi nautilus-image-converter dconf-editor &&
+apt install -y --ignore-missing gnome-tweaks gnome-shell-extensions cowsay lolcat neofetch htop discord tilix cura gnome-sushi nautilus-image-converter dconf-editor &&
 
 echo "Deleting line to disable Left Super opening the launcher"
 sed -i.bak -e '/    overview_toggle(overlay_key_action);/d' /usr/share/gnome-shell/extensions/pop-cosmic@system76.com/extension.js
-echo "Now installing ArcMenu and DashToPanel"
-wget -O gnome-shell-extension-installer "https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer" &&
+echo "Now installing extensions: ArcMenu and DashToPanel"
+wget -O /usr/bin/gnome-shell-extension-installer "https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer" &&
 chmod +x gnome-shell-extension-installer &&
-mv gnome-shell-extension-installer /usr/bin/ &&
-su -c "gnome-shell-extension-installer 3628" abel &&
+su -c "gnome-shell-extension-installer --restart-shell 3628" abel &&
 su -c "gnome-shell-extension-installer --restart-shell 1160" abel
